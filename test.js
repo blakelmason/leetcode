92d85139f8baa71399d03e8c24ce67c2b7f0fb7e
Blake Mason <blakelmason@gmail.com>
2020-11-16 20:24:31 -0700

    commit

---
+++ test.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+    const map = new Map
+    for (let i = 0; i < nums.length; i++) {
+        console.log(map)
+        const compare = target - nums[i]
+        if (map.has(compare)) {
+            console.log(map.get(compare))
+            return [map.get(compare), i]
+        }
+        map.set(nums[i], i)
+    }
+}
+
+twoSum([2,11,11,7], 9)
